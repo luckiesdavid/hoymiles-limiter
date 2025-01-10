e7b953fac2661f46987fd9b60907a3096203c867
david <114309176+luckiesdavid@users.noreply.github.com>
2025-01-10 12:36:54 +0100

    added hichi inverter, added logging info log

--- nulleinspeisung.py
+++ nulleinspeisung.py
@@ -2,17 +2,18 @@
 import requests, time, sys
 from requests.auth import HTTPBasicAuth
 import logging
+from datetime import datetime
+import os
 
-
-serials = [] 
+serials = []
 rec_serials = False
 max_power = []
 rec_max_power = False
 setpoint_factor = []
 rec_setpoint_factor = False
 old_limit = []
-rec_old_limit = False 
-#maximum_wr = 3500  # Maximale Ausgabe des Wechselrichters
+rec_old_limit = False
+# maximum_wr = 3500  # Maximale Ausgabe des Wechselrichters
 minimum_wr = 400  # Minimale Ausgabe des Wechselrichters
 offset_grid = -50
 
@@ -21,6 +22,7 @@ dtu_nutzer = 'xxx'  # OpenDTU Nutzername
 dtu_passwort = 'xxx'  # OpenDTU Passwort
 
 shelly_ip = '192.168.178.93'  # IP Adresse von Shelly 3EM
+hichi_ip = '10.0.1.130'
 
 grid_sum = None
 power = None
@@ -30,10 +32,20 @@ producing = None
 setpoint = None
 old_limit_all = None
 
+# Logging konfigurieren
+os.makedirs("logs", exist_ok=True)
+logging.basicConfig(
+    filename=os.path.join("logs", f'log_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log'),  # Name der Log-Datei
+    level=logging.INFO,           # Logging-Level (INFO, DEBUG, ERROR, etc.)
+    format='%(asctime)s - %(levelname)s - %(message)s'  # Format der Log-Nachrichten
+)
+
+
 def read_opendtu():
     global power, rec_serials, reachable, altes_limit, producing, old_limit, old_limit_all, rec_old_limit
     # Nimmt Daten von der openDTU Rest-API und übersetzt sie in ein json-Format
     r = requests.get(url=f'http://{dtu_ip}/api/livedata/status/inverters').json()
+    #print(r)
 
     # read_serials reading only once
     if rec_serials == False:
@@ -57,8 +69,8 @@ def read_opendtu():
             except:
                 print(f"Read {len(old_limit)} Inverter Limits ")
                 break
-        old_limit_all = sum(old_limit) #sum all limits
-    #print(f"Read {old_limit} Inverter Limits ")
+        old_limit_all = sum(old_limit)  # sum all limits
+    # print(f"Read {old_limit} Inverter Limits ")
 
     reachable = r['inverters'][0]['reachable']  # Ist DTU erreichbar?
     producing = int(r['inverters'][0]['producing'])  # Produziert der Wechselrichter etwas?
@@ -67,14 +79,16 @@ def read_opendtu():
     power = r['total']['Power']['v']  # Abgabe BKW AC in Watt
     print("...received opendDTU Data")
 
+
 def read_maxpower():
     global max_power, rec_max_power
     r = requests.get(url=f'http://{dtu_ip}/api/limit/status').json()
     for i in range(len(serials)):
-        #print(max_power)
-        #max_power[i] = r[serials[i]]['max_power']
+        # print(max_power)
+        # max_power[i] = r[serials[i]]['max_power']
         max_power.append(r[serials[i]]['max_power'])
         rec_max_power = True
+
 
 def inv_factor():
     global max_power_all, setpoint_factor, rec_setpoint_factor
@@ -84,17 +98,26 @@ def inv_factor():
     rec_setpoint_factor = True
     print(f"setpoint_factor: {setpoint_factor} ")
 
+
 def read_shelly():
     global grid_sum
     phase_a = requests.get(f'http://{shelly_ip}/emeter/0', headers={'Content-Type': 'application/json'}).json()['power']
     phase_b = requests.get(f'http://{shelly_ip}/emeter/1', headers={'Content-Type': 'application/json'}).json()['power']
     phase_c = requests.get(f'http://{shelly_ip}/emeter/2', headers={'Content-Type': 'application/json'}).json()['power']
-    grid_sum = phase_a + phase_b + phase_c 
+    grid_sum = phase_a + phase_b + phase_c
     print("...received Shelly 3EM Data")
+
+
+def read_hichi():
+    # 'http://10.0.1.130/cm?cmnd=Status%200'
+    global grid_sum
+    grid_sum = requests.get(f'http://{hichi_ip}/cm?cmnd=Status%200', headers={'Content-Type': 'application/json'}).json()['StatusSNS']['Power_cur']
+
 
 def set_limit():
     global setpoint
     print(f'Setze Inverterlimit von {round(altes_limit, 1)} W auf {round(setpoint, 1)} W... ')
+    logging.info(f'Setze Inverterlimit von {round(altes_limit, 1)} W auf {round(setpoint, 1)} W... ')
     for i in range(len(serials)):
         print(f"setpoint_all: {setpoint} setpoint_each: {setpoint * setpoint_factor[i]}")
     try:
@@ -104,12 +127,13 @@ def set_limit():
                 data=f'data={{"serial":"{serials[i]}", "limit_type":0, "limit_value":{setpoint * setpoint_factor[i]}}}',
                 auth=HTTPBasicAuth(dtu_nutzer, dtu_passwort),
                 headers={'Content-Type': 'application/x-www-form-urlencoded'}
-           )
-            print(r)
+            )
+            #print(r)
             print(f'Konfiguration gesendet ({r.json()["type"]})')
-        #print(f"gesendet{setpoint * setpoint_factor[i]}:")
+        # print(f"gesendet{setpoint * setpoint_factor[i]}:")
     except Exception as e:
         print(f'Fehler beim Senden der Konfiguration: \n {e}')
+        logging.error(f'Fehler beim Senden der Konfiguration: \n {e}')
 
 
 if __name__ == '__main__':
@@ -120,6 +144,7 @@ if __name__ == '__main__':
             if rec_max_power == False:
                 read_maxpower()
             read_shelly()
+            #read_hichi()
             if rec_setpoint_factor == False:
                 inv_factor()
         except Exception as e:
@@ -127,39 +152,52 @@ if __name__ == '__main__':
 
         ###### Simulation ######
         #reachable = True
-        #grid_sum = 1000
-        #altes_limit = 3500 #3500W = kein Limit // 0W = voll Limit
+        #grid_sum = -1000
+        ##altes_limit = 3500 #3500W = kein Limit // 0W = voll Limit
         #power = 2000
+        #pv1 = 500
+        #pv2 = 500
+        #pv3 = 500
+        #pv4 = 500
+        ##pv_all = []
+        #pv_all.append(pv1)
+        #pv_all.append(pv2)
+        #pv_all.append(pv3)
+        #pv_all.append(pv4)
+        #print(pv_all)
 
-        
+
         # Werte setzen
         print(f"Seriennummern: {serials} mit max_power: {max_power} ")
-        print(f'\nBezug: {round(grid_sum, 1)} W, Produktion: {round(power, 1)} W, altes_Limit: {round(old_limit_all, 1)} W')
+        print(
+            f'\nBezug: {round(grid_sum, 1)} W, Produktion: {round(power, 1)} W, altes_Limit: {round(old_limit_all, 1)} W')
+        logging.info(f'Bezug: {round(grid_sum, 1)} W, Produktion: {round(power, 1)} W, altes_Limit: {round(old_limit_all, 1)} W')
 
-        if len(max_power) & len(serials) == 4: # ATTENTION HARDCODED 4 INVERTERS!
+
+        if len(max_power) & len(serials) == 4:  # ATTENTION HARDCODED 4 INVERTERS!
             if reachable:
 
                 # Export
-                if grid_sum < offset_grid: 
-                    setpoint = (grid_sum + power) - offset_grid# *-1
+                if grid_sum < offset_grid:
+                    setpoint = (grid_sum + power) - offset_grid  # *-1
 
-                    #  upper Limit 
+                    #  upper Limit
                     if setpoint > max_power_all:
                         setpoint = max_power_all
                         print(f'Setpoint auf Maximum: {max_power_all} W')
-                    ## bottom Limit 
+                    ## bottom Limit
                     if setpoint < minimum_wr:
                         setpoint = minimum_wr
                         print(f'Setpoint auf Minimum: {minimum_wr} W')
                     # send limit
                     if setpoint != old_limit_all:
                         set_limit()
-                    
+
                 # no Export
                 if grid_sum >= offset_grid:
                     setpoint = max_power_all
                     print(f'no export -> no limit: {setpoint} W')
-                    
+
                     # old limit = new limit
                     if setpoint == old_limit_all:
                         print("Limits identical - not sending Limit")
@@ -169,8 +207,9 @@ if __name__ == '__main__':
 
             else:
                 print("not reachable")
+                logging.error("not reachable")
         else:
             print(f"Len max_power: {len(max_power)} or serials incorrect: {len(serials)}")
 
-        sys.stdout.flush() # write out cached messages to stdout
-        time.sleep(20) # wait
+        sys.stdout.flush()  # write out cached messages to stdout
+        time.sleep(20)  # wait
